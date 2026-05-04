b0b60d4b68c929f001bfb7d900e538712a444d09
niltonbrunes <niltonbrunes@gmail.com>
2026-05-03 23:02:08 -0300

    build: force update sidebar

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+// Layout de alto padrão Habita.vc - Atualizado para SaaS
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { 
